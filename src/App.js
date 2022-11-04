6818d02ff6254b048b40634c6dfddc1585482793
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-11-04 05:59:41 +0500

    Confetie  and game win logic added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Dice from './components/Dice';
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
 
-  const counter = 0;
   const [dieValue, setDiceValue] = useState(randomNumArray());
   const [tenzies, setTenzies] = useState(false);
+  const [count, setCount] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
 
   React.useEffect(() => {
     const allHeld = dieValue.every(die => die.isHeld)
@@ -15,7 +18,6 @@ function App() {
     const allSameValue = dieValue.every(die => die.value === firstValue)
     if (allHeld && allSameValue) {
       setTenzies(true)
-      console.log("You won!")
     }
   }, [dieValue])
 
@@ -52,18 +54,30 @@ function App() {
       <Dice key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />
     )
   });
-
+  const counter = 0;
 
   function rollDie() {
-    setDiceValue(dieValue.map(die => {
-      return die.isHeld ? die : generateDie()
+
+    if (!tenzies) {
+
+      setCount(count + 1)
+      setDiceValue(oldDice => oldDice.map(die => {
+        return die.isHeld ?
+          die :
+          generateDie()
+      }))
+    } else {
+      setCount(0)
+      setTenzies(false)
+      setDiceValue(randomNumArray())
     }
-    ))
   }
 
   const btnName = tenzies ? 'New Game' : 'Roll Dice';
   return (
     <main>
+
+      {tenzies && <Confetti />}
       <div className='about-game'>
         <h1 className='title'>Tenzies</h1>
         <p className='hint'>Roll untill all dice the same. Click each die to freeze it at its current value between rolls.</p>
@@ -71,13 +85,21 @@ function App() {
       <div className='die-container'>
         {dieElements}
       </div>
-      <button
-        onClick={() => rollDie()}
-        className='roll-btn'>
-        {btnName}
-      </button>
+      <div className='game-fact'>
+        <span className='rolls'>Min: rolls {highScore}</span>
+        <button
+          onClick={() => rollDie()}
+          className='roll-btn'>
+          {btnName}
+        </button>
+        <span className='your-rolls'>No. of rolls: {count}</span>
+
+      </div>
+
+
     </main>
   );
 }
 
 export default App;
+
